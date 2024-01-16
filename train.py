eedd1c80870ccfff9d3c1e4eb91345ed76e9301e
Sequel0x <AleksySi2509@protonmail.com>
2024-01-16 15:52:59 +0000

    [CODE] Added main function to train model + file checkpoint

--- train.py
+++ train.py
@@ -1,1 +1,27 @@
-# TO DO
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import ModelCheckpoint
+from datasets import SegDataModule
+from models import SegModel
+from lightning import seed_everything
+
+seed_everything(2023)
+
+def main():
+    datamodule = SegDataModule(batch_size=8)
+    model = SegModel()
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="checkpoints",
+        monitor="val_high_vegetation_IoU",
+        mode="max",
+        filename="{epoch}-{val_loss:.2f}-{val_high_vegetation_IoU:.2f}-{val_mIoU:.2f}",
+        save_top_k=3,
+    )
+
+    trainer = pl.Trainer(max_epochs=20, callbacks=[checkpoint_callback], log_every_n_steps=10)
+
+    trainer.fit(model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    main()
