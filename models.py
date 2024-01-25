319f50de81423197a7da4cb91380e7b1b187cdb3
Sequel0x <AleksySi2509@protonmail.com>
2024-01-25 17:42:05 +0000

    [CODE] Added further network functions

--- models.py
+++ models.py
@@ -32,3 +32,43 @@ class SegModel(nn.Module):
         self.losses = [nn.BCEWithLogitsLoss(posWeight=lw, reduction='none') for lw in lossWeights]
         self.losses = nn.ModuleList(self.losses)
 
+    def forward(self, x):
+        x = self.conv1(x)
+
+        downSampled = []
+        for i, layer in enumerate(self.downsampleBlocks):
+            x = layer(x)
+            downSampled.append(x)
+
+            x = nn.MaxPool2d(2, stride=2)(x)
+
+        multiscalePred = []
+        x = self.upsampleBlocks[0](x)
+        x = nn.Upsample(scaleFactor=2.0, mode='nearest')(x)
+        for i, layer in enumerate(self.upsampleBlocks[1:]):
+            x = x + downSampled[-i - 1]
+            x = layer(x)
+
+            multiscalePred.append(self.preLossConv[i](x))
+            x = nn.Upsample(scaleFactor=2.0, mode='nearest')(x)
+        
+        return multiscalePred
+
+    def createRCBlock(self, resConvBlocksPerLayer):
+        blocks = []
+        for i in range(resConvBlocksPerLayer):
+            blocks.append(nn.LeakyReLU(inplace=False))
+            blocks.append(nn.Conv2d(self.channelNum, self.channelNum, (3, 3), padding=1))
+
+        return nn.Sequential(*blocks)
+
+    def computeMultiscaleLoss(self, multiscalePred, multiscaleTarget, multiscaleMasks):
+        losses = [torch.sum(self.losses[i](x, y) * mask) / torch.sum(mask) for i, (x, y, mask) in
+                    enumerate(zip(multiscalePred, multiscaleTarget, multiscaleMasks))]
+            
+        return sum(losses)
+
+    if __name__ == '__main__':
+        neuralnet = SegModel(5, lossWeights=[torch.tensor([0.1]), torch.tensor([0.4]), torch.tensor([1.]),
+                                      torch.tensor([4]), torch.tensor([10])])
+        
