59dcee767d0c22ec3d709771441e1bece72453d4
Sequel0x <AleksySi2509@protonmail.com>
2024-01-25 17:02:57 +0000

    [CODE]  Inital start on pyramid like model

--- models.py
+++ models.py
@@ -1,61 +1,34 @@
 import torch
 import torch.nn as nn
-import torchvision
-import lightning.pytorch as pl
-from metrics import SMAPIoUMetric
 
+class SegModel(nn.Module):
 
-class SegModel(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, nLayers, inputChannels=3, outputChannels=1, lossWeights=None):
         super(SegModel, self).__init__()
-        self.learning_rate = 1e-3
-        self.net = torchvision.models.segmentation.fcn_resnet50(num_classes=2)
-        self.criterion = nn.CrossEntropyLoss()
-        self.evaluator = SMAPIoUMetric()
+        self.channelNum = 32
+        self.resConvBlocksPerLayer = 4
 
-    def forward(self, x):
-        return self.net(x)
+        # First Convolution
+        self.conv1 = nn.Conv2d(inputChannels, self.channelNum, (3, 3), padding=1)
 
-    def training_step(self, batch, batch_nb):
-        img, mask = batch
-        img = img.float()
-        mask = mask.long()
-        out = self.forward(img)["out"]
-        loss = self.criterion(out, mask)
-        self.log("loss", loss, prog_bar=True, sync_dist=True)
-        return loss
+        # NN Structure
+        self.upsampleBlocks = []
+        self.downsampleBlocks = []
+        self.preLossConv = []
+        for i in range(nLayers):
+            self.upsampleBlocks.append(self.createRCBlock(self.resConvBlocksPerLayer))
+            self.downsampleBlocks.append(self.createRCBlock(self.resConvBlocksPerLayer))
+            self.preLossConv.append(nn.Sequential(
+                nn.ReLU(inplace=False),
+                nn.Conv2d(self.channelNum, outputChannels, 3, padding=1)
+            ))
 
-    def validation_step(self, batch, batch_nb):
-        img, mask = batch
-        img = img.float()
-        mask = mask.long()
-        out = self.forward(img)["out"]
-        loss = self.criterion(out, mask)
+        self.upsampleBlocks.append(self.createRCBlock(self.resConvBlocksPerLayer))
 
-        probs = torch.softmax(out, dim=1)
-        preds = torch.argmax(probs, dim=1)
-        preds = preds.detach().cpu().numpy()
-        mask = mask.detach().cpu().numpy()
+        self.upsampleBlocks = nn.ModuleList(self.upsampleBlocks)
+        self.downsampleBlocks = nn.ModuleList(self.downsampleBlocks)
+        self.preLossConv = nn.ModuleList(self.preLossConv)
 
-        self.evaluator.process(input={"pred": preds, "gt": mask})
+        self.losses = [nn.BCEWithLogitsLoss(posWeight=lw, reduction='none') for lw in lossWeights]
+        self.losses = nn.ModuleList(self.losses)
 
-        self.log(
-            "val_loss",
-            loss,
-            prog_bar=True,
-            sync_dist=True,
-            on_step=False,
-            on_epoch=True,
-        )
-
-    def on_validation_epoch_end(self) -> None:
-        metrics = self.evaluator.evaluate(0)
-        self.log(
-            f"val_high_vegetation_IoU",
-            metrics["high_vegetation__IoU"],
-            sync_dist=True,
-        )
-        self.log(f"val_mIoU", metrics["mIoU"], sync_dist=True)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.net.parameters(), lr=self.learning_rate)
